5a66321bfc0db63cd1abec4bbc1f344e68f71271
Ikhsan N. Rosyidin <ikhsan.rosyidin@traveloka.com>
2024-04-25 01:50:46 +0700

    Optimize import

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import logging
 import os
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://{uname}:{passwd}@{host}/{dbname}".format(
